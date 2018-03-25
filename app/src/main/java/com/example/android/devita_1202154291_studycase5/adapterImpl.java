8cee0271d85032c28e4f409260b15f64b3e8c407
ayudevita <ayudevita31@gmail.com>
2018-03-25 21:21:17 +0700

    push push

---
+++ app/src/main/java/com/example/android/devita_1202154291_studycase5/adapterImpl.java
@@ -0,0 +1,85 @@
+package com.example.android.devita_1202154291_studycase5;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by lenovo on 3/25/2018.
+ */
+
+class adapterImpl extends adapter {
+    public class adapter extends RecyclerView.Adapter<com.example.android.devita_1202154291_studycase5.adapter.holder> {
+        //deklarasi variable yang akan digunakan
+        private Context cntx;
+        private List<AddData> list;
+        int color;
+
+        //konstruktor
+        public adapter(Context cntx, List<AddData> list, int color){
+            this.cntx=cntx;
+            this.list=list;
+            this.color=color;
+        }
+
+        //menentukan viewholder untuk recyclerview
+        @Override
+        public com.example.android.devita_1202154291_studycase5.adapter.holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            //membuat view baru
+            View view = LayoutInflater.from(cntx).inflate(R.layout.cardview_list, parent, false);
+            com.example.android.devita_1202154291_studycase5.adapter.holder hldr = new com.example.android.devita_1202154291_studycase5.adapter.holder(view);
+            return hldr;
+        }
+
+        //menyetting nilai yang didapatkan pada viewholder
+        @Override
+        public void onBindViewHolder(com.example.android.devita_1202154291_studycase5.adapter.holder holder, int position) {
+            AddData data = list.get(position);
+            holder.ToDo.setText(data.getTodo());
+            holder.Description.setText(data.getDesc());
+            holder.Priority.setText(data.getPrior());
+            holder.cardv.setCardBackgroundColor(cntx.getResources().getColor(this.color));
+        }
+
+        //mendapatkan jumlah list
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        //mendapatkan list dari adapter
+        public AddData getData(int position){
+            return list.get(position);
+        }
+
+        //untuk menghapus list pada todolist
+        public void deleteData(int i){
+            //remove item yang terpilih
+            list.remove(i);
+            //memberi notifikasi item yang di remove
+            notifyItemRemoved(i);
+            notifyItemRangeChanged(i, list.size());
+        }
+
+        private void notifyItemRemoved(int i) {
+        }
+
+        class holder extends RecyclerView.ViewHolder{
+            //deklarasi variable yang akan digunakan
+            public TextView ToDo, Description, Priority;
+            public CardView cardv;
+            public holder(View itemView){
+                super(itemView);
+
+                //mengakses id text view pada layout dan juga cardview
+                ToDo = itemView.findViewById(R.id.headline);
+                Description = itemView.findViewById(R.id.explanation);
+                Priority = itemView.findViewById(R.id.number);
+                cardv = itemView.findViewById(R.id.cardlist);
+            }
+        }
+    }
